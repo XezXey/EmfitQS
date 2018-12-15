c4b18ac9615b7831b970639ade47ce2e6e9f6764
XezXey <puntawat-1@hotmail.com>
2018-12-15 21:16:47 +0700

    Adding auto create subfolder for each subject(argv[1] as name)

--- get_emfitqs_data.py
+++ get_emfitqs_data.py
@@ -5,9 +5,13 @@ import pandas as pd
 import time
 import _thread
 import matplotlib.pyplot as plt
+import os
+import sys
+
 
 exit_flag = 0
 emfit_qs_value = {}
+path = './dataset_emfitqs/'
 
 def input_thread():
     global exit_flag 
@@ -15,6 +19,7 @@ def input_thread():
     
 exit_flag = _thread.start_new_thread(input_thread, ( ))
 start_time = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+path = path + sys.argv[1] + '_' + start_time + '/'
 
 while True:
     emfit_qs_value.setdefault('timestamp_from_machine', [])
@@ -30,7 +35,18 @@ while True:
     if exit_flag == 1:
         break
 
-print("Writing data...", end='') 
+# Trying to make directory if it's not exist
+if not os.path.exists(os.path.dirname(path)):
+    try:
+        os.makedirs(os.path.dirname(path))
+    except OSError as exc: #Guard against race condition
+        if exc.errno != errno.EEXIST:
+            raise
+
+print("Writing data...", end='')
+
+
+filename = sys.argv[1] + '_' + start_time + "_emfit_data.csv"
 emfit_df = pd.DataFrame.from_dict(emfit_qs_value)
-emfit_df.to_csv(start_time + "_emfit_data.csv")
+emfit_df.to_csv(path + filename)
 print("Done!")
